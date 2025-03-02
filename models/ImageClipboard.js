de1c094056a15700c4fd318828bf4c25e7921073
Gautam Makwana <makwanagautam411@gmail.com>
2025-03-02 09:23:05 +0530

    temp

--- models/ImageClipboard.js
+++ models/ImageClipboard.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const ImageClipboardSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true }, // Cloudinary URL
+const imageClipboardSchema = new mongoose.Schema({
+  imageUrl: { type: String, required: true },
   code: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now, expires: 600 }, // Auto delete after 10 min
+  createdAt: { type: Date, default: Date.now, expires: 600 }, // Auto-delete in 10 mins
 });
 
-module.exports = mongoose.model("ImageClipboard", ImageClipboardSchema);
+module.exports = mongoose.model("ImageClipboard", imageClipboardSchema);
