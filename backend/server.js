fb20539340880e37ee78f8f8f9df679fbbee51ed
Gautam Makwana <makwanagautam411@gmail.com>
2025-03-04 07:42:44 +0530

    temp

--- backend/server.js
+++ backend/server.js
@@ -54,5 +54,5 @@ cron.schedule("*/10 * * * *", async () => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
