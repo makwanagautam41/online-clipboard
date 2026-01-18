c0d632fb1655b308eb65dff34a5f5912ac665d81
Gautam Makwana <makwanagautam411@gmail.com>
2026-01-18 19:53:59 +0530

    fix routing path for Hp component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="hp" element={<Hp />} />
+          <Route path="/hp" element={<Hp />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
